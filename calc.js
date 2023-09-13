a995554ef721a563c20a5ba62ee1b70ad8729098
ebdiego <ebdiego@iu.edu>
2023-09-13 15:58:57 -0400

    homework3

--- calc.js
+++ calc.js
@@ -2,5 +2,5 @@ function interestAdd() {
     var principalInput = document.getElementById('pInput').value;
     var interestInput = document.getElementById('iInput').value;
     var timeInput = document.getElementById('tInput').value;
-    document.getElementById('interestResults').innerHTML = "With a beginning principal of  " + principalInput + "and with a growth rate of  " + interestInput + "for " + timeInput + "years, your total interest will be " + (principalInput * 1 *interestInput * timeInput);
+    document.getElementById('interestResults').innerHTML = "With a beginning principal of "+ " " + principalInput + "and with a growth rate of  " + " " + interestInput + "for " + " " + timeInput + "years, your total interest will be " + " " + (principalInput * 1 *interestInput * timeInput);
 }
